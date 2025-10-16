23ea3730de3b8bd65a3d6540700546dbcdc1b7f4
rozhan28 <rozhanarya1@gmail.com>
2025-10-16 21:42:37 +0200

    UC19 Product in database en nieuw product aanmaken, toegevoegd

--- Grocery.App/ViewModels/ProductViewModel.cs
+++ Grocery.App/ViewModels/ProductViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using Grocery.Core.Interfaces.Services;
 using Grocery.Core.Models;
+using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace Grocery.App.ViewModels
 {
@@ -9,11 +11,20 @@ namespace Grocery.App.ViewModels
         private readonly IProductService _productService;
         public ObservableCollection<Product> Products { get; set; }
 
+        public IAsyncRelayCommand AddNewProductCommand { get; }
+
         public ProductViewModel(IProductService productService)
         {
             _productService = productService;
-            Products = [];
-            foreach (Product p in _productService.GetAll()) Products.Add(p);
+
+            Products = new ObservableCollection<Product>(_productService.GetAll());
+
+            AddNewProductCommand = new AsyncRelayCommand(GoToNewProductViewAsync);
+        }
+
+        private async Task GoToNewProductViewAsync()
+        {
+            await Shell.Current.GoToAsync(nameof(Grocery.App.Views.NewProductView));
         }
     }
 }
